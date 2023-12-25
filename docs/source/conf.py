89e5d449da712df2a437295790f8f8e777317546
Jose Garcia Herruzo <jgherruzo@gmail.com>
2023-12-25 21:39:10 +0100

    RnR row 137 lenth modified

---
+++ docs/source/conf.py
@@ -0,0 +1,42 @@
+# Originaly from Juan Luis Cano Rodríguez
+# https://www.youtube.com/watch?v=qRSb299awB0&t=1700s
+#
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+from pathlib import Path
+
+project = "pyPETB"
+copyright = "2023, Jose Garcia"
+author = "Jose Garcia"
+release = "0.1.0"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "myst_parser",
+    "sphinx.ext.duration",
+    "sphinx.ext.autosectionlabel",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "autoapi.extension",
+    "nbsphinx",
+]
+autoapi_type = "python"
+autoapi_dirs = [f"{Path(__file__).parents[2]}/src"]
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
---
+++ docs/source/index.md
@@ -0,0 +1,18 @@
+% pyPETB documentation master file, created by
+% sphinx-quickstart on Sun Dec 24 17:13:50 2023.
+% You can adapt this file completely to your liking, but it should at least
+% contain the root `toctree` directive.
+
+# Welcome to pyPETB's documentation!
+
+```{toctree}
+:caption: 'Contents:'
+:maxdepth: 2
+notebooks/usage
+```
+
+# Indices and tables
+
+- {ref}`genindex`
+- {ref}`modindex`
+- {ref}`search`
