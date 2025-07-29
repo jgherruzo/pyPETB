623217a31820c7ed637df911bef59af264c8ce0c
Jose García Herruzo <jgherruzo@gmail.com>
2025-07-29 21:41:34 +0200

    pypetb release updated

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ from pathlib import Path
 project = "pyPETB"
 copyright = "2023, Jose Garcia"
 author = "Jose Garcia"
-release = "0.5.1"
+release = "1.0.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
